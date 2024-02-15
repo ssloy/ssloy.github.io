0df4e3620bb4e36385bf12c82355f42ca6f4094f
Dmitry V. Sokolov <ha@haqr.eu>
2024-02-15 16:57:02 +0100

    sopfr

--- docs/tinycompiler/transpy-novars/sopfr-inspect.py
+++ docs/tinycompiler/transpy-novars/sopfr-inspect.py
@@ -1,9 +1,14 @@
+import inspect
+
 def main():
     def sopfr(n):
         def sopfr_aux(n):
             nonlocal div
             rec = 0
             if n % div == 0:
+                # breakpoint, print stack
+                var = {"main":[], "sopfr":["n","div"], "sopfr_aux":["n","rec"]}
+                print([frame[0].f_locals[name] for frame in reversed(inspect.stack()[:-1]) for name in var[frame[3]]])
                 rec = div
                 if n != div:
                     rec = rec + sopfr_aux(n // div)
