4c7d7725b112f3ad32056314cdf163a60076a691
Dmitry V. Sokolov <ha@haqr.eu>
2026-01-22 12:49:44 +0100

    convergence

---
+++ docs/banana/target/convergence-x-bisection.py
@@ -0,0 +1,62 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+random.seed(1)
+
+plt.figure(figsize=(16, 9))
+plt.rcParams["font.family"] = "serif"
+plt.rcParams["mathtext.fontset"] = "dejavuserif"
+plt.rcParams['text.usetex'] = True
+
+plt.rc('font', size=34)
+plt.title(f'Convergence of the bisection method')
+plt.grid(color='gray', linestyle='--', linewidth=0.5)
+
+
+
+g     = -9.81 # gravity, m/s^2
+angle = 60    # launch angle, degrees wrt the ground
+def x_impact(v0):
+    x, y = 0, 0   # current state
+    vx, vy = v0 * np.cos(angle * np.pi/180), \
+             v0 * np.sin(angle * np.pi/180)
+    dt = .001
+    xs, ys = [x], [y]
+    while y >= 0:
+        x += dt * vx
+        y += dt * vy
+        vy += g * dt
+        xs.append(x)
+        ys.append(y)
+    u = ys[-2]/(ys[-2]-ys[-1])
+    xs[-1] = xs[-2] + u*(xs[-1]-xs[-2])
+    ys[-1] = 0
+    return xs[-1]
+
+for _ in range(100):
+    xstar = random.uniform(1, 100)
+    a, b = 0, 35
+    err = []
+    while True:
+        m = (a + b)/2
+        x = x_impact(m)
+        if x < xstar:
+            a = m
+        else:
+            b = m
+        err.append(abs(x-xstar))
+        if err[-1] < 1e-3:
+            v0 = m
+            break
+    plt.plot(range(1,len(err)+1), err,c='gray',alpha=.1)
+
+plt.ylim((1e-4,50))
+plt.xlim((0,15))
+plt.xlabel("Number of bananas launched")
+plt.ylabel("Distance to the target, meters")
+plt.yscale('log')
+
+#plt.gca().set_aspect('equal', 'box')
+plt.savefig("convergence-x-bisection.png")
+plt.show()
+
